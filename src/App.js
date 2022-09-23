a2595077ff6c1925ac2e418f0bd1843649c6e7d3
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-24 01:34:00 +0900

    fixed online badge bug

--- src/App.js
+++ src/App.js
@@ -9,28 +9,29 @@ import data from "./data"
 function App() {
 
   const cards = data.map(item => {
-    return(
+    return (
       <Card
-      key={item.id}
-      img={item.coverImg}
-      rating={item.stats.rating}
-      reviewCount={item.stats.reviewCount}
-      country={item.location}
-      title={item.title}
-      price={item.price}
-    />
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
     )
   })
 
   return (
     <div>
-              <div>
-                <Navbar />
-                <Hero />
-                <section className='card-list'>
-                    {cards}
-                </section>
-              </div>
+      <div>
+        <Navbar />
+        <Hero />
+        <section className='card-list'>
+          {cards}
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,26 @@ import React from "react"
 import Star from '../assets/star.png'
 
 export default function Card(props) {
-    return (
 
-        <section>
-            <div className="card">
-                <img src={`/images/${props.img}`} alt="katie" className="card--photo" />
-                    <div className="review">
-                        <img src={Star} alt="star" className="card--star"/>
-                        <p>{props.rating} ({props.reviewCount}) {props.country}</p>
-                    </div>
-                <p>{props.title}</p>
-                <p><strong>From ${props.price} / </strong> person</p>
-                <span className="sold">SOLD OUT</span>
-            </div>
-        </section>
-    );
+    let badgeText
+    if (props.openSpots === 0) {
+      badgeText = "SOLD OUT";
+    } else if (props.country === "Online") {
+      badgeText = "ONLINE";
+    }
+  return (
+
+    <section>
+      <div className="card">
+        <img src={`/images/${props.img}`} alt="katie" className="card--photo" />
+        <div className="review">
+          <img src={Star} alt="star" className="card--star" />
+          <p>{props.rating} ({props.reviewCount}) {props.country}</p>
+        </div>
+        <p>{props.title}</p>
+        <p><strong>From ${props.price} / </strong> person</p>
+        {badgeText && <div className="sold">{badgeText}</div>}
+      </div>
+    </section>
+  );
 }
