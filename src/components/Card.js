bc0701ce2438ee6988a4fa9db6f15565fcd4c9a5
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-25 14:31:53 +0900

    cleared up props

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,22 +4,24 @@ import Star from '../assets/star.png'
 export default function Card(props) {
 
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
       badgeText = "SOLD OUT";
-    } else if (props.country === "Online") {
+    } else if (props.item.location === "Online") {
       badgeText = "ONLINE";
     }
+
+
   return (
 
     <section>
       <div className="card">
-        <img src={`/images/${props.img}`} alt="katie" className="card--photo" />
+        <img src={`/images/${props.item.coverImg}`} alt="katie" className="card--photo" />
         <div className="review">
           <img src={Star} alt="star" className="card--star" />
-          <p>{props.rating} ({props.reviewCount}) {props.country}</p>
+          <p>{props.item.stats.rating} ({props.item.stats.reviewCount}) {props.item.location}</p>
         </div>
-        <p>{props.title}</p>
-        <p><strong>From ${props.price} / </strong> person</p>
+        <p>{props.item.title}</p>
+        <p><strong>From ${props.item.price} / </strong> person</p>
         {badgeText && <div className="sold">{badgeText}</div>}
       </div>
     </section>
